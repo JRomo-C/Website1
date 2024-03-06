728e84fd56b21c3d48507988da4cf82a3bc1b5bb
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-03-06 17:07:45 +0000

    Wed, Mar 6, 2024, 9:07 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', fixnav)
 
 function fixnav() {
     console.log(window.scrollY)
-    if(window.scroll > 150) {
+    if(window.scroll > 220) {
         nav.classList.add('active')
     }
     else{
